676544602611c5bcade683a6f6e6f0d484c8806c
SongMing <mmsong@yeah.net>
2025-07-02 21:02:14 +0800

    nuxt template init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,16 @@
 // @ts-check
-import stylistic from '@stylistic/eslint-plugin'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-  // Your custom configs here
-  stylistic.configs.customize({
-    indent: 2,
-    quotes: 'single',
-    semi: false,
-  }),
   {
+    files: ['pages/**/*.vue', 'layouts/**/*.vue'],
     rules: {
       // 允许在 Vue 组件中定义多个根元素
       'vue/no-multiple-template-root': ['off'],
+      // 允许在 Vue 组件中使用单个单词单词的组件名
+      'vue/multi-word-component-names': ['off'],
+      // 使用单引号
+      'quotes': ['error', 'single'],
     },
   },
 )
--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -2,6 +2,7 @@
  * 全局路由鉴权
  *
  */
-export default defineNuxtRouteMiddleware((_to, _from) => {
+export default defineNuxtRouteMiddleware((to, from) => {
   // isAuthenticated() is an example method verifying if a user is authenticated
+  console.log('全局路由鉴权', to, from)
 })
