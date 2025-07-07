e920e7d74884c8cf27fa1f1a3994eddf521f3f53
songmm <songmm@qingsongjituan.com>
2025-07-07 10:41:06 +0800

    github action deploy

--- middleware/route.global.ts
+++ middleware/route.global.ts
@@ -1,8 +1,9 @@
 /**
  * 全局路由鉴权
  *
+ * @see https://nuxt.com/docs/guide/directory-structure/middleware
  */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default defineNuxtRouteMiddleware((to, from) => {
   // isAuthenticated() is an example method verifying if a user is authenticated
-  console.log('全局路由鉴权', to, from)
 })
